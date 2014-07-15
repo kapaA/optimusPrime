7fd98b5262cbc070e73d54c5dfe51ce4be5ba810
Achuthan Paramanathan <ap@es.aau.dk>
2014-07-15 20:56:06 +0200

    Added blink function
    Added blink function

--- optimusPrime.py
+++ optimusPrime.py
@@ -1,5 +1,9 @@
 import RPi.GPIO as GPIO
+import time
+import thread
+from random import randint
 
+eye_color = 'BLUE'
 
 def gpio_initializing():
     GPIO.setmode(GPIO.BOARD)
@@ -17,7 +21,7 @@ def face_initializing():
     GPIO.setup(22, GPIO.OUT) # BLUE
     
     #Turn On blue eye
-    set_eye_color('BLUE')
+    set_eye_color(eye_color)
    
 def set_eye_color(eye):
     if ('RED' == eye):
@@ -33,10 +37,27 @@ def set_eye_color(eye):
         GPIO.output(18, False) # GREEN
         GPIO.output(22, True) # BLUE
 
+def blink():
+    while(1):
+        t = randint(2,5)
+        time.sleep(t)
+        GPIO.output(16, False) # RED
+        GPIO.output(18, False) # GREEN
+        GPIO.output(22, False) # BLUE
+        time.sleep(1)
+        set_eye_color(eye_color)
+        
+        print "blink"
+        
 def main():
-    # initialization phase
+    #initialization phase
     gpio_initializing()
     face_initializing()
+    try:
+        thread.start_new_thread( blink() ) 
+    except:
+        print "Error: unable to start thread"
+
 
 
 if __name__ == "__main__":
